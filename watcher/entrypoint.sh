08438d6848ca977011606df9ea5897fa51c5805e
joeonome <joseph.onumeguolor@gmail.com>
2025-11-03 21:34:14 +0100

    fix: forgot to commit entrypoint.sh (corrected)

---
+++ watcher/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "Installing dependencies..."
+pip install -r /app/requirements.txt
+
+echo "Waiting for Nginx logs to be ready..."
+while [ ! -f /var/log/nginx/access.log ]; do
+    echo "[!] Log file not found, retrying in 5s..."
+    sleep 5
+done
+
+echo "Starting watcher..."
+python /app/watcher.py
